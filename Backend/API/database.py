4b90c4d5097ff4ba553390f6d84b75990382b101
Leevi Hokkanen <leevi.sahkoposti@gmail.com>
2023-02-11 03:15:17 +0200

    Set up database connection for SQLAlchemy.

---
+++ Backend/API/database.py
@@ -0,0 +1,22 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+try:
+    PSQL_USER = os.environ['PSQL_USER']
+    PSQL_PWD = os.environ['PSQL_PWD']
+    PSQL_DOMAIN = os.environ['PSQL_DOMAIN']
+    PSQL_DB = os.environ['PSQL_DB']
+except KeyError as e:
+    raise KeyError(f'Environment variable is not set: {e}. It is required to '
+                    'connect to the database.') from e
+
+PSQL_DATABASE_URL =(
+    f'postgresql://{PSQL_USER}:{PSQL_PWD}@{PSQL_DOMAIN}/{PSQL_DB}'
+)
+
+engine = create_engine(PSQL_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
