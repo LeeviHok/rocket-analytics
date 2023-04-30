ebc84673ea5e2b932679c3687f1e0b981c3422e2
Leevi Hokkanen <leevi.sahkoposti@gmail.com>
2023-05-01 02:00:00 +0300

    refactor: Combines flight record and data models.
    Flight record and flight data are combined on API level, and these need
    to be combined also on database level.

--- backend/api/api/crud.py
+++ backend/api/api/crud.py
@@ -7,25 +7,20 @@ from . import models, schemas
 from .standard_atmosphere import StandardAtmosphere as SA
 
 def create_flight(db: Session, data: schemas.FlightCreate):
-    metadata_fields = {'rocket_name', 'flight_datetime'}
-    flight_metadata = models.FlightRecord(**data.dict(include=metadata_fields))
-    flight_data = models.FlightData(**data.dict(exclude=metadata_fields))
+    flight = models.Flight(**data.dict())
 
     # Calculate and add derived data
-    flight_data.altitude_asl = []
-    flight_data.altitude_agl = []
-    pressure_ground = flight_data.pressure[0]
-    for pressure in flight_data.pressure:
+    flight.altitude_asl = []
+    flight.altitude_agl = []
+    pressure_ground = flight.pressure[0]
+    for pressure in flight.pressure:
         altitude_asl = SA.get_altitude_asl(pressure)
         altitude_agl = SA.get_altitude_agl(pressure, pressure_ground)
-        flight_data.altitude_asl.append(altitude_asl)
-        flight_data.altitude_agl.append(altitude_agl)
+        flight.altitude_asl.append(altitude_asl)
+        flight.altitude_agl.append(altitude_agl)
 
-    db.add(flight_metadata)
+    db.add(flight)
     try:
-        db.commit()
-        flight_data.flight_record_id = flight_metadata.id
-        db.add(flight_data)
         db.commit()
     except IntegrityError:
         db.rollback()
@@ -33,49 +28,23 @@ def create_flight(db: Session, data: schemas.FlightCreate):
             status_code=400, detail='Failed to create flight record.'
         )
 
-    db.refresh(flight_metadata)
-    return flight_metadata
-
-def get_flight(db: Session, flight_id: int):
-    flight_metadata = get_flight_metadata(db, flight_id)
-    flight_data = flight_metadata.flight_data
-    if not flight_data:
-        raise HTTPException(
-            status_code=404,
-            detail='Flight data does not exist for given flight record.'
-        )
-
-    flight = {}
-    flight['id'] = flight_metadata.id
-    flight['rocket_name'] = flight_metadata.rocket_name
-    flight['flight_datetime'] = flight_metadata.flight_datetime
-    flight['time'] = flight_data.time
-    flight['accel_axial'] = flight_data.accel_axial
-    flight['accel_lateral'] = flight_data.accel_lateral
-    flight['pressure'] = flight_data.pressure
-    flight['current'] = flight_data.current
-    flight['temperature'] = flight_data.temperature
-    flight['velocity'] = flight_data.velocity
-    flight['voltage_battery'] = flight_data.voltage_battery
-    flight['voltage_pyro_apogee'] = flight_data.voltage_pyro_apogee
-    flight['voltage_pyro_main'] = flight_data.voltage_pyro_main
-    flight['voltage_pyro_3'] = flight_data.voltage_pyro_3
-    flight['voltage_pyro_4'] = flight_data.voltage_pyro_4
-    flight['altitude_asl'] = flight_data.altitude_asl
-    flight['altitude_agl'] = flight_data.altitude_agl
-
+    db.refresh(flight)
     return flight
 
-def get_flight_metadata_list(db: Session):
-    return db.scalars(
-        select(models.FlightRecord).order_by(models.FlightRecord.id)
-    ).all()
-
-def get_flight_metadata(db: Session, flight_id):
-    flight_metadata = db.get(models.FlightRecord, flight_id)
-    if not flight_metadata:
+def get_flight(db: Session, flight_id: int):
+    flight = db.get(models.Flight, flight_id)
+    if not flight:
         raise HTTPException(
             status_code=404,
             detail='Flight record with given ID does not exist.'
         )
-    return flight_metadata
+    return flight
+
+def get_flight_metadata_list(db: Session):
+    return db.execute(
+        select(
+            models.Flight.id,
+            models.Flight.flight_datetime,
+            models.Flight.rocket_name,
+        ).order_by(models.Flight.flight_datetime)
+    ).all()
--- backend/api/api/models.py
+++ backend/api/api/models.py
@@ -1,36 +1,19 @@
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, Numeric, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, DateTime, Integer, Numeric, String
 from sqlalchemy.types import ARRAY
 
 from .database import Base
 
 
-class FlightRecord(Base):
-    __tablename__ = 'flight_records'
+class Flight(Base):
+    __tablename__ = 'flights'
 
+    # Metadata
     id = Column(Integer, primary_key=True, index=True)
-    flight_data = relationship(
-        'FlightData', back_populates='flight_record', uselist=False
-    )
-
     rocket_name = Column(String, nullable=False)
     flight_datetime = Column(DateTime(timezone=True), nullable=False)
     #flight_description = ?
 
-    def __repr__(self):
-        return self.rocket_name
-
-
-class FlightData(Base):
-    __tablename__ = 'flight_data'
-
-    id = Column(Integer, primary_key=True, index=True)
-    flight_record_id = Column(
-        Integer, ForeignKey('flight_records.id'), unique=True, nullable=False
-    )
-    flight_record = relationship('FlightRecord', back_populates='flight_data')
-
-    # Measured data from the altimeter
+    # Flight data (measured by the altimeter)
     time = Column(ARRAY(Numeric(9, 5)), nullable=False)                # s
     accel_axial = Column(ARRAY(Numeric(10, 7)), nullable=False)        # G
     accel_lateral = Column(ARRAY(Numeric(10, 7)), nullable=False)      # G
@@ -47,3 +30,6 @@ class FlightData(Base):
     # Derived data from the altimeter measurements
     altitude_asl = Column(ARRAY(Numeric(9, 3)), nullable=False)        # m
     altitude_agl = Column(ARRAY(Numeric(9, 3)), nullable=False)        # m
+
+    def __repr__(self):
+        return self.rocket_name
