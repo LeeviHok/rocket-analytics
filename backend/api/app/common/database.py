912f5c1cf03598c9d7172f6fd3da420d0ad3550f
Leevi Hokkanen <leevi.sahkoposti@gmail.com>
2024-02-18 11:45:30 +0200

    refactor: Improves naming in docker compose files.

--- backend/api/app/common/database.py
+++ backend/api/app/common/database.py
@@ -4,10 +4,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 try:
-    PSQL_USER = os.environ['PSQL_USER']
-    PSQL_PWD = os.environ['PSQL_PWD']
-    PSQL_DOMAIN = os.environ['PSQL_DOMAIN']
-    PSQL_DB = os.environ['PSQL_DB']
+    PSQL_USER = os.environ['POSTGRES_USERNAME']
+    PSQL_PWD = os.environ['POSTGRES_PASSWORD']
+    PSQL_DOMAIN = os.environ['POSTGRES_DOMAIN']
+    PSQL_DB = os.environ['POSTGRES_DATABASE']
 except KeyError as e:
     raise KeyError(f'Environment variable is not set: {e}. It is required to '
                     'connect to the database.') from e
